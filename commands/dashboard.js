4d509de2a995b337094d36d9216626beb91308f9
Jesper Joergensen <jesper@heroku.com>
2016-02-13 01:18:12 -0800

    add support for Linux and Windows

--- commands/dashboard.js
+++ commands/dashboard.js
@@ -10,6 +10,14 @@ module.exports = {
   help: 'help text for hello:world',
   needsApp: true,
   run: function (context) {
-    exec("open https://dashboard.heroku.com/apps/"+context.app)
+    if(process.platform == "darwin") {
+      exec("open https://dashboard.heroku.com/apps/"+context.app)
+    } else if(process.platform == "win32") {
+      exec("start https://dashboard.heroku.com/apps/"+context.app)
+    } else if(process.platform == "linux"){
+      exec("xdg-open https://dashboard.heroku.com/apps/"+context.app)
+    } else {
+      console.log("Sorry. Don't know how to open a browser on your OS: "+process.platform)
+    }
   }
 };
