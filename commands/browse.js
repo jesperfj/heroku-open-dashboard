6c834b2fe5a15fabd53dfcd7b38b2a577253ec79
Jakub Janczak <jakub.janczak@schibsted.pl>
2018-03-22 10:20:55 +0100

    Adding app subpages

--- commands/browse.js
+++ commands/browse.js
@@ -2,22 +2,27 @@
 
 let exec = require('child_process').exec;
 
-module.exports = {
+const openApp = (app, subpage) => {
+  const url = "https://dashboard.heroku.com/apps/"+app+(subpage ? "/"+subpage : "");
+    if(process.platform == "darwin") {
+      exec("open "+url)
+    } else if(process.platform == "win32") {
+      exec("start "+url)
+    } else if(process.platform == "linux"){
+      exec("xdg-open "+url)
+    } else {
+      console.log("Sorry. Don't know how to open a browser on your OS: "+process.platform)
+    }
+};
+
+module.exports = (subpagecommand) => ({
   topic: 'browse',
-  command: 'overview',
-  description: 'Opens the app overview page for the current app in your default browser',
+  command: subpagecommand,
+  description: `Opens the app ${subpagecommand} page for the current app in your default browser`,
   default: true,
   help: '',
   needsApp: true,
   run: function (context) {
-    if(process.platform == "darwin") {
-      exec("open https://dashboard.heroku.com/apps/"+context.app)
-    } else if(process.platform == "win32") {
-      exec("start https://dashboard.heroku.com/apps/"+context.app)
-    } else if(process.platform == "linux"){
-      exec("xdg-open https://dashboard.heroku.com/apps/"+context.app)
-    } else {
-      console.log("Sorry. Don't know how to open a browser on your OS: "+process.platform)
-    }
+    openApp(context.app, subpagecommand);
   }
-};
+});
